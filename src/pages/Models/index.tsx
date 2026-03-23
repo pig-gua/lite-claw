52f47ca7439e3f3aba37d88fdb79d1826bf2cdeb
dengyuxuan <1270701867@qq.com>
2026-03-23 17:48:01 +0800

    feat(Models): 添加模型卡片组件展示模型信息

--- src/pages/Models/index.tsx
+++ src/pages/Models/index.tsx
@@ -1,7 +1,33 @@
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from "@ant-design/icons";
+import { Avatar, Card } from "antd";
+
+const { Meta } = Card;
+
 const ModelsPage = () => {
   return (
     <div>
       Models
+      <Card
+        style={{ width: 256 }}
+        cover={
+          <img
+            draggable={false}
+            alt="example"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          />
+        }
+        actions={[
+          <SettingOutlined key="setting" />,
+          // <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+      >
+        <Meta
+          // avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+          title="Qwen-2.5-Chat-Plus-1"
+          description="千问"
+        />
+      </Card>
     </div>
   );
 };
